7237183bbb2873f409ae0033abf4a5c456f5f91b
pandaMary <vlksm.33@gmail.com>
2021-03-11 18:53:51 +0100

    init: Typical arrays problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
 
 exports.min = function min (array) {
-  return 0;
+	if (!array || !array.length) {
+		return 0;
+	}
+
+	let res = array[0];
+
+	for (let i = 0; i < array.length; i++) {
+		if (array[i] < res) {
+			res = array[i];
+		}
+	}
+
+	return res;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array && array.length) {
+        let res = array[0];
+
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] > res) {
+                res = array[i];
+            }
+        }
+
+        return res;
+    } else {
+        return 0;
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (!array || !array.length) {
+		return 0;
+}
+
+    let sum = array.reduce(function(sum, current) {
+        return sum + current;
+    }, 0);
+
+    return(sum / array.length);
 }
